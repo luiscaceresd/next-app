b1bf28c973970f0dc19b33f4313f1ed179ad9738
luiscaceresd <luiscaceresd97@gmail.com>
2024-05-11 21:47:31 -0300

    adding google social login

---
+++ middleware.ts
@@ -0,0 +1,10 @@
+export { default } from "next-auth/middleware";
+
+export const config = {
+  // *: zero or more
+  // +: one or more
+  // ?: zero or one
+  matcher: [
+    '/dashboard/:path*',
+  ]
+}
